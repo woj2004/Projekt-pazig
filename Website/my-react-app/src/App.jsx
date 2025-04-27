f924238f4b169555cce9851f68e4cffe1453ec47
woj2004 <wojtekp_2004@onet.pl>
2025-04-27 20:03:28 +0200

    Logowanie do apki
    Logowanie do aplikacji + połączenie z firebasem (wstępne)

--- Website/my-react-app/src/App.jsx
+++ Website/my-react-app/src/App.jsx
@@ -1,25 +1,51 @@
-import Calendar from "./Calendar";
-import NavBar from "./navbar";
-import { useState } from "react";
+import React, { useEffect } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+
+import Login from "./Login";
+import SignUp from "./Register";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Profile from "./Profile";
+import { useState } from "react";
+import { auth } from "./Firebase";
 
 function App() {
-
-  const [patients, setPatients] = useState([
-    { id: 1, imie: "Anna", nazwisko: "Kowalska" },
-    { id: 2, imie: "Jan", nazwisko: "Nowak" },
-    { id: 3, imie: "Katarzyna", nazwisko: "Wiśniewska" },
-  ]);
-  
-  return(
-    <div className="app-background">
-      <div style={{ marginBottom: '2rem' }}>
-       <NavBar />
+  const [user, setUser] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
+  return (
+    <Router>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Navigate to="/profile" /> : <Login />}
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+            <ToastContainer />
+          </div>
+        </div>
       </div>
-        <Calendar patients={patients} setPatients={setPatients} />
-    </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
